02f8e6acc47c88ef7a53b0b08a09a67ddff68d4c
rajesh23-drg <rmanjunath@teamdrg.com>
2018-05-14 10:31:11 +0530

    Getting State Access in HTTP Interceptor

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -2,20 +2,31 @@
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent} from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
-
-import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
+//import { AuthService } from '../auth/auth.service';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-  constructor(private authService: AuthService){}
+ //private authService: AuthService,
+  constructor( private store: Store<fromApp.AppState>){}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // by default requests are immutable(CAN'T EDIT) hevce clone request
     console.log('Intercepted!', req);
-    // we can only read not write, hence we can send an object to clone
-    const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
-    //const copiedReq = req.clone({headers: req.headers.append('', '')});
-    return next.handle(copiedReq);
+    //// we can only read not write, hence we can send an object to clone
+    //const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
+    ////const copiedReq = req.clone({headers: req.headers.append('', '')});
+    //return next.handle(copiedReq);
+
+    // map will return an observable and wrap the return into a new observable,
+    // by using switchMap it wont wrap
+    return this.store.select('auth')
+               .switchMap((authState: fromAuth.State) => {
+                  const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
+                  return next.handle(copiedReq);
+               })
   }
 }
