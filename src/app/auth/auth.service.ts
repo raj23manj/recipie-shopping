2c18019c88f2fdc7b38974533efbb5d56da63eda
rajesh23-drg <rmanjunath@teamdrg.com>
2018-05-14 09:55:45 +0530

    Dispatch actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,26 @@
 import * as firebase from 'firebase';
 import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as fromAuth from '../store/app.reducers';
+import * as AuthActions from './store/auth.actions';
 
 @Injectable()
 export class AuthService {
 
-  token: string;
+  //token: string;
 
-  constructor(private router: Router){
+  constructor(private router: Router, private store: Store<fromAuth.AppState>){
 
   }
 
   signupUser(email: string, password: string){
     // console.log('fine');
     firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(user => {
+              this.store.dispatch(new AuthActions.Signup());
+            })
             .catch(error => console.log(error));
   }
 
@@ -21,36 +28,39 @@ export class AuthService {
     // console.log('fine');
     firebase.auth().signInWithEmailAndPassword(email, password)
             .then((response) => {
+              this.store.dispatch(new AuthActions.Signin());
               this.router.navigate(['/']);
               console.log(response)
               firebase.auth().currentUser.getToken()
                       .then(token => {
-                        this.token = token;
+                        //this.token = token;
+                        this.store.dispatch(new AuthActions.SetToken(token));
                       });
             })
             .catch(error => console.log(error));
   }
 
-  getToken(){
-    firebase.auth().currentUser.getToken()
-                    .then((response) => {
-                      console.log(response)
-                      firebase.auth().currentUser.getToken()
-                              .then(token => {
-                                this.token = token;
-                              });
-                    });
-
-    return this.token;
-  }
+  // getToken(){
+  //   firebase.auth().currentUser.getToken()
+  //                   .then((response) => {
+  //                     console.log(response)
+  //                     firebase.auth().currentUser.getToken()
+  //                             .then(token => {
+  //                               this.token = token;
+  //                             });
+  //                   });
+  //
+  //   return this.token;
+  // }
 
   logout(){
     firebase.auth().signOut();
-    this.token = null;
+    //this.token = null;
+    this.store.dispatch(new AuthActions.Logout());
   }
 
-  isAuthenticated(){
-    return this.token != null;
-  }
+  // isAuthenticated(){
+  //   return this.token != null;
+  // }
 
 }
