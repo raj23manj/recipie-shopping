1bda8cc99913174e7892d55b7deb625c840a8cec
rajesh23-drg <rmanjunath@teamdrg.com>
2018-05-15 11:23:20 +0530

    Additional Fixes about take

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromApp from '../store/app.reducers';
 import * as fromAuth from './store/auth.reducers';
+import { take } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -13,7 +14,8 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
     // return this.authService.isAuthenticated();
-    return this.store.select('auth').map((authState: fromAuth.State) => {
+    // take only one request, if not it will run infinetly
+    return this.store.select('auth').take(1).map((authState: fromAuth.State) => {
       return authState.authenticated;
     });
   }
